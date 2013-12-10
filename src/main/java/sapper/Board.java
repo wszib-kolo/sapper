5879d5062252d7886d7facf2b1213fb898cf85d4
Łukasz Job <job.lukasz.1990@gmail.com>
2013-12-10 10:41:25 +0100

    add { } in change flag status

--- src/main/java/sapper/Board.java
+++ src/main/java/sapper/Board.java
@@ -22,8 +22,9 @@ public class Board implements Serializable {
 	}
 
 	public boolean changeFlagStatus(int x, int y) {
-		if (!isWin())
+		if (!isWin()){
 			flagField(x, y, !isFlaged(x, y));
+		}
 		return isFlaged(x, y);
 	}
 
