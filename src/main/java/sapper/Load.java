6cfc7af1fe52f9c52657dd278eab4bdf053850f2
ulysses1900 <juras.jan@gmail.com>
2013-12-13 18:08:15 +0100

    Save and Load functionality tests (regular testng, no mockito yet). One test for both saving and loading. Let me know if testing two functionalities in one test is ok. If not ok, any tips on how to do it better are more than welcome.

--- src/main/java/sapper/Load.java
+++ src/main/java/sapper/Load.java
@@ -5,16 +5,16 @@ import java.io.ObjectInputStream;
 
 public class Load{
 
-	public Board loadFromFile() {
-		Board board = null;
+	public static BoardAndCounter loadFromFile() {
+		BoardAndCounter boardAndCounter = null;
 		try {
 			FileInputStream fileStream = new FileInputStream("MySapper.ser");
 			ObjectInputStream is = new ObjectInputStream(fileStream);
-			board = (Board) is.readObject();
+			boardAndCounter = (BoardAndCounter) is.readObject();
 			is.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return board;
+		return boardAndCounter;
 	}
 }
