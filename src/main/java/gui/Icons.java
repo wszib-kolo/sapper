e9afce830b19cf5862a087b674ee83777b317367
unijewski <greg0907@gmail.com>
2013-12-13 20:43:38 +0100

    Moved the icons from SapperGui class to an interface.

---
+++ src/main/java/gui/Icons.java
@@ -0,0 +1,24 @@
+package gui;
+
+import javax.swing.ImageIcon;
+
+public interface Icons {
+	String prefix = "icons/";
+	public static ImageIcon zeroBomb = new ImageIcon(prefix + "0.png");
+	public static ImageIcon oneBomb = new ImageIcon(prefix + "1.png");
+	public static ImageIcon twoBombs = new ImageIcon(prefix + "2.png");
+	public static ImageIcon threeBombs = new ImageIcon(prefix + "3.png");
+	public static ImageIcon fourBombs = new ImageIcon(prefix + "4.png");
+	public static ImageIcon fiveBombs = new ImageIcon(prefix + "5.png");
+	public static ImageIcon sixBombs = new ImageIcon(prefix + "6.png");
+	public static ImageIcon sevenBombs = new ImageIcon(prefix + "7.png");
+	public static ImageIcon eightBombs = new ImageIcon(prefix + "8.png");
+	public static ImageIcon flag = new ImageIcon(prefix + "flag.png");
+	public static ImageIcon explode = new ImageIcon(prefix + "expl.png");
+	public static ImageIcon bomb = new ImageIcon(prefix + "bomb.png");
+	public static ImageIcon flaggedBomb = new ImageIcon(prefix + "bombflag.png");
+	public static ImageIcon win = new ImageIcon(prefix + "win.png");
+	public static ImageIcon clean = new ImageIcon(prefix + "clean.png");
+	public static ImageIcon badFlagged = new ImageIcon(prefix + "badflag.png");
+
+}
