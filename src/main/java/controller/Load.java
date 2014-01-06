0dd8d3b249dcb9a2ad95722014f44bef02b74fdc
ulysses1900 <juras.jan@gmail.com>
2014-01-06 18:01:52 +0100

    Added loading file with option of file selection. Throws exception (does not work properly)

--- src/main/java/controller/Load.java
+++ src/main/java/controller/Load.java
@@ -2,21 +2,37 @@ package controller;
 
 import java.io.FileInputStream;
 import java.io.ObjectInputStream;
+import java.io.File;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 import sapper.BoardAndCounter;
 
-public class Load{
+public class Load {
 
 	public static BoardAndCounter loadFromFile() {
 		BoardAndCounter boardAndCounter = null;
 		try {
-			FileInputStream fileStream = new FileInputStream("MySapper.ser");
-			ObjectInputStream is = new ObjectInputStream(fileStream);
-			boardAndCounter = (BoardAndCounter) is.readObject();
-			is.close();
+			FileNameExtensionFilter filter = new FileNameExtensionFilter(
+					"Saper saved games", "ser");
+			JFrame parentFrame = new JFrame();
+			JFileChooser loadFileChooser = new JFileChooser();
+			loadFileChooser.addChoosableFileFilter(filter);
+			loadFileChooser.setDialogTitle("Choose a game ...");
+			int userSelection = loadFileChooser.showOpenDialog(parentFrame);
+
+			if (userSelection == JFileChooser.APPROVE_OPTION) {
+				File f = loadFileChooser.getSelectedFile();
+				FileInputStream fileStream = new FileInputStream(f);
+				ObjectInputStream is = new ObjectInputStream(fileStream);
+				boardAndCounter = (BoardAndCounter) is.readObject();
+				is.close();
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		System.out.println("returnign BoardAndCounter ...");
 		return boardAndCounter;
 	}
 }
